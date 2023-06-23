5b19f6e56bdef1c79773c13a3a21ca9591e621c7
Prek16 <126586962+Prek16@users.noreply.github.com>
2023-06-23 20:17:04 +0530

    Add files via upload

---
+++ Bikerproblem.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Preksha Agrawal
+ */
+    import java.util.*;
+public class Bikerproblem {
+
+  public static void main(String[] args)
+  {
+         Scanner sc=new Scanner(System.in);
+         int no=sc.nextInt();
+          String bin="";
+         
+          while(no!=0)
+           {
+                  bin=(no&1)+bin;
+                  no=no>>1;
+           }
+            bin=bin.replaceAll("1","2");
+            bin=bin.replaceAll("0","1");
+            bin=bin.replaceAll("2","0");
+            int res=Integer.parseInt(bin,2);
+           System.out.println(res);
+   }
+}
+}
